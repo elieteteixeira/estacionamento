c1416f70becbe3d833197769b79a5cb522b29552
gerlande varanda <93235555+Gerlande-varanda@users.noreply.github.com>
2021-11-30 22:55:37 -0300

    alterações nas classes
    alteração nas classes cliente e funcionário

--- classes/cliente.py
+++ classes/cliente.py
@@ -1,15 +1,39 @@
 
 class cliente:
 
-    def __init__(self, nome, cpf, endereco, telefone):
+    def __init__(self, nome, cpf, endereco, contato):
 
         self.__nome = nome
         self.__cpf = cpf
         self.__endereco = endereco
-        self.__telefone = telefone
+        self.__contato = contato
+        self.__veiculos = []
 
-    def __str__(self) -> str:
-        return f'Nome: {self.__nome} cpf: {self.__cpf} endereco: {self.__endereco} telefone: {self.__telefone}'
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @property
+    def contato(self):
+        return self.__contato
+
+    def cadastrarVeiculo(self, veiculo):
+        self.__veiculos.append(veiculo)
+        print('o veiculo  modelo {}, foi cadastrado em nome do cliente {}'.format(veiculo.descricao, self.__nome))
+
+
+    def __str__(self):
+        cl = 'cliente :\n'
+        s1 = (' nome :{}\ncpf :{}\ncontato :{}\n'.format(self.__nome,self.__cpf,self.__contato))
+        s2 = 'relação dos veiculos de {}'.format(self.__nome)+'\n'
+        s3 = " "
+        for i in range(len(self.__veiculos)):
+            s3 = s3+'veiculo :'+self.__veiculos[i].descricao+ ' ' +'placa :'+self.__veiculos[i].placa+'\n'
+        return cl+s1+s2+s3
 
 
          
--- classes/funcionario.py
+++ classes/funcionario.py
@@ -1,11 +1,29 @@
 
 class funcionario:
-    def __init__(self, nome, cpf, endereco, telefone,matricula):
+    def __init__(self, nome, cpf, endereco, telefone, matricula, funcao):
         self.__nome = nome
         self.__cpf = cpf
         self.__endereco = endereco
         self.__telefone = telefone
         self.__matricula = matricula
+        self.__funcao = funcao
+
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @property
+    def matricula(self):
+        return self.__matricula
+
+    @property
+    def funcao(self):
+        return self.__funcao
 
     def __str__(self) -> str:
-        return f'Nome: {self.__nome} CPF: {self.__cpf} Endereco: {self.__endereco} Telefone: {self.__telefone} Matricula: {self.__matricula}'
+        return f'Nome: {self.__nome}\nCPF: {self.__cpf}\nEndereco: {self.__endereco}\nTelefone: {self.__telefone}\nMatricula: {self.__matricula}\nFunção: {self.__funcao}'
